08077e4f31fa1555f06ae810d3be4ede14453de3
Russell Bie <russell.bie@hulu.com>
2021-03-13 13:29:11 +0800

    disable scoverage for scala 2.12

--- core/src/main/scala/com/hulu/neutrino/macro/SerializableProxy.scala
+++ core/src/main/scala/com/hulu/neutrino/macro/SerializableProxy.scala
@@ -2,7 +2,6 @@ package com.hulu.neutrino.`macro`
 
 import com.hulu.neutrino.lang.JSerializable
 
-import java.io.Serializable
 import scala.language.experimental.macros
 
 object SerializableProxy {
